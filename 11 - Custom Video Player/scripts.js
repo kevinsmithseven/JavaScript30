6843e728358387f20b7163de6ce121fa8ed00886
kevinsmithseven <kevinsmithseven@gmail.com>
2024-01-16 15:18:29 -0600

    completed js30 module 10

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,42 @@
+/* Get our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* Build out functions */
+
+function togglePlay() {
+    // const method = video.paused ? 'play' : 'pause';
+    // video[method]();
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton() {
+    // const icon = this.paused ? '►' : '❚ ❚';
+    // toggle.textContent = icon;
+    if (video.paused) {
+        toggle.textContent = '►';
+    } else {
+        toggle.textContent = '❚❚';
+    }
+}
+
+function skip() {
+
+}
+
+
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+skipButtons.forEach(button => button.addEventListener('click', skip));
